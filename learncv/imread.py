a9af822834a39f51f3b4d94b531f1e324f464280
yashaka <yashaka@gmail.com>
2024-06-20 23:01:37 +0300

    NEW: add imread.gray

--- learncv/imread.py
+++ learncv/imread.py
@@ -1,7 +1,13 @@
 import cv2
 
+
 def RGB(path):
     return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
 
+
 def RGBs(path):
     return cv2.split(RGB(path))
+
+
+def gray(path):
+    return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)
